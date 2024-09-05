498ef5173973220c15bfa90d463738d117b6d51a
Felipe Torres Cresto <felipe.t.cresto@gmail.com>
2024-09-05 02:32:37 -0300

    feat: add register and login redirect

--- src/app/ui/auth-ui/auth-ui.component.ts
+++ src/app/ui/auth-ui/auth-ui.component.ts
@@ -1,11 +1,12 @@
 import { Component, inject, Input } from '@angular/core';
 import { LoaderComponent } from "../../shared/loader/loader.component";
 import { LoaderService } from '../../core/signals/loader.service';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-auth-ui',
   standalone: true,
-  imports: [LoaderComponent],
+  imports: [LoaderComponent, RouterLink, RouterLinkActive],
   templateUrl: './auth-ui.component.html',
   styleUrl: './auth-ui.component.scss'
 })
